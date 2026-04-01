08cfe3582285e0f8a7af83ea0a478701cd439c5b
belteshazzar525 <belteshazzar525@proton.me>
2026-04-01 16:49:48 -0400

    WIP: bind support - Maintain parents when an observer is added or removed

--- Incremental/Bind.lean
+++ Incremental/Bind.lean
@@ -3,6 +3,8 @@ import Std.Data.HashSet.Basic
 
 abbrev NodeId := Nat
 
+abbrev EdgeMap := Std.HashMap NodeId (List NodeId)
+
 abbrev StabilizeId := Nat
 
 inductive Scope where
@@ -20,7 +22,7 @@ inductive NodeTy γ where
   | map : NodeId → (Float → Float) → NodeTy γ
   | map2 : NodeId → NodeId → (Float → Float → Float) → NodeTy γ
   | map3 : NodeId → NodeId → NodeId → (Float → Float → Float → Float) → NodeTy γ
-  | bind : NodeId → (Float → γ × NodeId) → NodeTy γ
+  | bind : NodeId → Option NodeId → (Float → γ × NodeId) → NodeTy γ
 
 instance : ToString (NodeTy γ) where
   toString t := match t with
@@ -54,12 +56,16 @@ def Node.toGraphviz (n : Node γ) : String :=
   | .map a _ => nodeStr ++ s!"\n  {n.id} -> {a} [constraint=\"true\"]"
   | .map2 a b _ => nodeStr ++ s!"\n  {n.id} -> {a} [constraint=\"true\"]\n  {n.id} -> {b} [constraint=\"true\"]"
   | .map3 a b c _ => nodeStr ++ s!"\n  {n.id} -> {a} [constraint=\"true\"]\n  {n.id} -> {b} [constraint=\"true\"]\n  {n.id} -> {c} [constraint=\"true\"]"
-  | .bind a _ => nodeStr ++ s!"\n  {n.id} -> {a} [constraint=\"true\"]"
+  | .bind a rhs _ => nodeStr ++
+    s!"\n  {n.id} -> {a} [constraint=\"true\"]" ++
+    if let some b := rhs then
+      s!"\n  {n.id} -> {b} [constraint=\"true\"]"
+    else ""
 
 structure Graph where
   nodes : List (Node Graph)
   observers : Std.HashSet NodeId
-  parents : Std.HashMap NodeId (List NodeId)
+  parents : EdgeMap
   varChanges : Std.HashMap NodeId Float
   stabilizeId : StabilizeId
   currentScope : Scope
@@ -161,7 +167,7 @@ def Graph.bind (g : Graph) (a : NodeId) (f : Float → GraphChange) (label : Str
   let newId := g.numNodes
   let newBind := {
     id := newId
-    ty := NodeTy.bind a f
+    ty := NodeTy.bind a none f
     label
     value := 0.0
     createdIn := g.currentScope
@@ -171,6 +177,54 @@ def Graph.bind (g : Graph) (a : NodeId) (f : Float → GraphChange) (label : Str
 
   let newG := { g with nodes := g.nodes.concat newBind }
   (newG, newId)
+
+/-- NOTE:
+1. We can make this non-partial
+2. Perf opportunities from building up a new EdgeMap from scratch vs mutating w/ one-reference
+3. Initial EdgeMap is not initialized to parents' capacity, because abbrev's confound me
+-/
+partial def Graph.maintainParents (g : Graph) : Graph :=
+  let addParent (parents : EdgeMap) (c : NodeId) (p : NodeId) : EdgeMap :=
+    parents.alter c fun ps =>
+      match ps with
+      | none => some [p]
+      | some ps => some (ps.concat p)
+
+  let rec maintainParents (toVisit : List NodeId) (visited : List NodeId) (parents : EdgeMap) : EdgeMap :=
+    match toVisit with
+    | [] => parents
+    | n :: remainingNodes =>
+      if visited.contains n then
+        panic! s!"Cycle detected to {n}"
+      else
+        let (children, newParents) := match g.nodes[n]!.ty with
+        | .var => ([], parents)
+        | .map a _ =>
+          ([a], addParent parents a n)
+        | .map2 a b _ =>
+          ([a, b], addParent (addParent parents a n) b n)
+        | .map3 a b c _ =>
+          ([a, b, c], addParent (addParent (addParent parents a n) b n) c n)
+        | .bind a rhs _ =>
+          if let some b := rhs then
+            ([a, b], addParent (addParent parents a n) b n)
+          else
+            ([a], addParent parents a n)
+
+        let newToVisit := remainingNodes ++ children.filter (!remainingNodes.contains ·)
+        maintainParents newToVisit (visited.concat n) newParents
+
+  { g with parents := maintainParents g.observers.toList [] {} }
+
+def Graph.observe (g : Graph) (n : NodeId) : Graph :=
+  if !g.observers.contains n then
+    { g with observers := g.observers.insert n }.maintainParents
+  else g
+
+def Graph.unobserve (g : Graph) (n : NodeId) : Graph :=
+  if g.observers.contains n then
+    { g with observers := g.observers.erase n }.maintainParents
+  else g
 
 def Graph.setVar (g : Graph) (v : NodeId) (value : Float) : Graph :=
   { g with varChanges := g.varChanges.insert v value }
@@ -208,23 +262,16 @@ def makeStaticIfElse : StaticIfElse :=
     if x == 1.0 then y else z)
     "result"
 
-  -- PICKUP: make necessary and parents happen
-  let necessaryParents: Std.HashMap NodeId (List NodeId) := {
-    (a, [cond]),
-    (b, [ifBranch, elseBranch]),
-    (c, [ifBranch]),
-    (d, [elseBranch]),
-    (cond, [result]),
-    (ifBranch, [result]),
-    (elseBranch, [result])
-  }
-  let g := { g with parents := necessaryParents }
-
   ⟨g, a, b, c, d, result⟩
 
 def staticIfElse := makeStaticIfElse
--- #eval staticIfElse.g.stabilizeState.recomputeHeap
-#eval IO.println staticIfElse.g
+#eval staticIfElse.g
+
+def observingResult := staticIfElse.g.observe staticIfElse.result
+#eval observingResult
+
+def stopObservingResult := staticIfElse.g.unobserve staticIfElse.result
+#eval stopObservingResult
 
 end StaticIfElseTest
 
@@ -261,6 +308,6 @@ def makeDynamicIfElse : DynamicIfElse :=
   ⟨g, a, b, c, d, result⟩
 
 def dynamicIfElse := makeDynamicIfElse
-#eval IO.println dynamicIfElse.g.toGraphviz
+#eval IO.println dynamicIfElse.g
 
 end DynamicIfElseTest
